85b93b9e7fd640a17b10b61d6e05fabe50ad9f4a
Geoff Cox <gcox@microsoft.com>
2015-08-13 11:21:15 -0700

    Improved docked layout

--- BnLCharts/bnlCharts/directives/bnl-docked-layout.js
+++ BnLCharts/bnlCharts/directives/bnl-docked-layout.js
@@ -1,4 +1,39 @@
-﻿angular.module('bnlCharts')
+﻿// ================================================================================ //
+/*
+ * BnL Charts - bnl-docked-layout
+ * 
+ * Provides layout of child SVG elements based on docking to the sides of the available
+ * area with undocked element taking up the remaining space.
+ * 
+ * Usage
+ * -----
+ * HTML:
+ * 
+ *   <bnl-chart ...>
+ *     ...
+ *     <bnl-docked-layout>                
+ *       <bnl-y-axis ... dock="left" width="25" margin="0,0,25,0"></bnl-y-axis>
+ *       <bnl-x-axis ... dock="bottom" height="25"></bnl-x-axis>                               
+ *       <bnl-area ... ></bnl-area>
+ *     </bnl-docked-layout>
+ *   </bnl-chart>
+ * 
+ *   The optional dock element can be top, left, right, or bottom.
+ *   If multiple items at the same dock position overlap.
+ *   You can nest bnl-docked-layout elements to stack element docked to the same side.
+ *   
+ *   The optional margin element allows spacing around elements.  
+ *   Margin behaves like the CSS margin attribute (i.e. TRBL)
+ *    
+ * 
+ * Design Considerations
+ * ---------------------
+ * Elements are positioned using tranform: translate
+ * Width and Height are set on the scope of each child element.
+ * Child elements are expected to have isolated scope.
+ */
+// ================================================================================ //
+angular.module('bnlCharts')
 .directive('bnlDockedLayout', function () {
 
     var parseMargin = function (text) {
@@ -90,6 +125,20 @@
         return margin;
     }
 
+    var positionChild = function (child, x, y, width, height) {
+
+        if (x !== 0 || y !== 0) {
+            d3.select(child).attr({
+                transform: 'translate(' + x + ',' + y + ')'
+            });
+        }
+
+        var childScope = angular.element(child).isolateScope();
+
+        childScope.width = width;
+        childScope.height = height;
+    }
+
     return {
         controller: function ($scope, $element, $attrs, $transclude) {
             console.log('bnlDockedLayout controller:' + $scope.$id);
@@ -110,61 +159,72 @@
 
                 var x = 0, y = 0, width = $scope.width ? $scope.width : svg.clientWidth, height = $scope.height ? $scope.height : svg.clientHeight;
 
+                var undocked = [];
+                var undockedMargin = { top: 0, right: width, bottom: height, left: 0 };
+
                 $.each($($element).children(), function (index, child) {
 
                     var $child = $(child);
-                    var dock = $child.attr('dock');
 
-                    var childX = x, childY = y, childWidth = width, childHeight = height;
+                    var childMargin = parseMargin($child.attr('margin'));
+
+                    var childX = x + childMargin.left;
+                    var childY = y + childMargin.top;
+                    var childWidth = $child.attr('width') ? Number($child.attr('width')) : width;
+                    var childHeight = $child.attr('height') ? Number($child.attr('height')) : height;
+
+                    var childDockHeight = childMargin.top + childHeight + childMargin.bottom;
+                    var childDockWidth = childMargin.left + childWidth + childMargin.right;
+
+                    var dock = $child.attr('dock');
+                    if (dock) {
+                        switch (dock) {
+                            case 'top':
+                                undockedMargin.top = Math.max(y + childDockHeight, undockedMargin.top);
+                                childWidth -= childMargin.left + childMargin.right;
+                                break;
+                            case 'right':
+                                undockedMargin.right = Math.min(width - childDockWidth, undockedMargin.right);
+                                childX = width - childDockWidth;
+                                childHeight -= childMargin.top + childMargin.bottom;
+                                break;
+                            case 'bottom':
+                                undockedMargin.bottom = Math.min(height - childDockHeight, undockedMargin.bottom);
+                                childY = height - childDockHeight;
+                                childWidth -= childMargin.left + childMargin.right;
+                                break;
+                            case 'left':
+                                undockedMargin.left = Math.max(x + childDockWidth, undockedMargin.left);
+                                childHeight -= childMargin.top + childMargin.bottom;
+                                break;
+                            default:
+                                console.log('The dock attribute "' + dock + '" is not supported by bnl-docked-layout. The element will be treated as undocked.');
+                                dock = undefined;
+                                break;
+                        }
+                    }
 
                     if (dock) {
-                        if (dock === 'top') {
-                            childHeight = $child.attr('height') ? Number($child.attr('height')) : height;
-                            y += childHeight;
-                            height -= childHeight;
-                        }
-                        else if (dock === 'right') {
-
-                            childWidth = $child.attr('width') ? Number($child.attr('width')) : width;
-                            childX = width - childWidth;
-
-                            width -= childWidth;
-                        }
-                        else if (dock === 'bottom') {
-
-                            childHeight = $child.attr('height') ? Number($child.attr('height')) : height;
-                            childY = height - childHeight;
-
-                            height -= childHeight;
-                        }
-                        else if (dock === 'left') {
-                            childWidth = $child.attr('width') ? Number($child.attr('width')) : width;
-
-                            x += childWidth;
-                            width -= childWidth;
-                        }
+                        positionChild(child, childX, childY, childWidth, childHeight);
                     }
-
-                    // I support margin attribute (TRBL same as CSS margin
-                    var margin = parseMargin($child.attr('margin'));
-                    childY += margin.top;
-                    childX += margin.left;
-
-                    childHeight -= (margin.top + margin.bottom);
-                    childWidth -= (margin.left + margin.right);
-
-                    // I position each child
-                    if (childX !== 0 || childY !== 0) {
-                        d3.select(child).attr({
-                            transform: 'translate(' + childX + ',' + childY + ')'
-                        });
+                    else {
+                        undocked.push({ child: child, childMargin: childMargin });
                     }
-
-                    var childScope = angular.element(child).isolateScope();
-
-                    childScope.width = childWidth;
-                    childScope.height = childHeight;
                 });
+
+                // undocked children are positioned after undockedMargin is calculated               
+                for (var i = 0; i < undocked.length; i++) {
+
+                    var child = undocked[i].child;
+                    var childMargin = undocked[i].childMargin;
+
+                    var childX = undockedMargin.left + childMargin.left;
+                    var childY = undockedMargin.top + childMargin.top;
+                    var childWidth = (undockedMargin.right - childMargin.right) - childX;
+                    var childHeight = (undockedMargin.bottom - childMargin.bottom) - childY;
+
+                    positionChild(child, childX, childY, childWidth, childHeight);
+                }
             });
         },
         replace: true,
